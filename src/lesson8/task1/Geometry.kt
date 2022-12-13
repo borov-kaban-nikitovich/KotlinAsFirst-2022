732fed658fd54dfd00105ee4b67a59a645f1fa65
Sergey <nesterenko.sa@edu.spbstu.ru>
2022-12-13 17:29:13 +0300

    lesson 8. minContainingCircle not solved :(

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -312,26 +312,4 @@ fun circleByThreePoints(a: Point, b: Point, c: Point): Circle {
  * три точки данного множества, либо иметь своим диаметром отрезок,
  * соединяющий две самые удалённые точки в данном множестве.
  */
-fun minContainingCircle(vararg points: Point): Circle {
-    if (points.isEmpty())
-        throw IllegalArgumentException("At least 1 point required")
-    if (points.size == 1)
-        return Circle(points[0], 0.0)
-    val diameter = diameter(*points)
-    // Окружность, построенная на отрезке между самыми удалёнными точками
-    // множества, как на радиусе, точно содержит все точки множества
-    var minCircle = Circle(diameter.begin, diameter.length)
-    for (i in points.indices)
-        for (j in i + 1..points.lastIndex)
-            for (k in j + 1..points.lastIndex) {
-                val circle = circleByThreePoints(points[i], points[j], points[k])
-                if (circle.radius < minCircle.radius && points.all { circle.contains(it) })
-                    minCircle = circle
-            }
-    val circleByTwoPoints = circleByDiameter(diameter)
-    if (circleByTwoPoints.radius < minCircle.radius && points.all { circleByTwoPoints.contains(it) })
-        minCircle = circleByTwoPoints
-    return minCircle
-    // Примечание: функция не проходит некоторые тесты из-за погрешностей вычислений. Чтобы увеличить их
-    // точность, для предыдущих задач нужно реализовать менее очевидные решения, что усложнит их восприятие.
-}
+fun minContainingCircle(vararg points: Point): Circle = TODO()
